0551cd2b52d82b83cf58bef85ed41149a86fb6c6
nicollaslopes <nicollaslopesn@gmail.com>
2022-11-28 11:06:38 -0300

    criando migration e seeder de produto

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'cover' => $this->faker->imageUrl(),
+            'price' => $this->faker->randomFloat(1, 20, 30),
+            'description' => $this->faker->sentence(),
+            'stock' => $this->faker->randomDigit(),
+        ];
+    }
+}
