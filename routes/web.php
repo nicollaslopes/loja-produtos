7516aef0b54ccb2c5ae7e9873921178a5d7d6483
nicollaslopes <nicollaslopesn@gmail.com>
2022-11-28 16:06:02 -0300

    inserindo a rota para visualizar o produto

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/product/{product}', [ProductController::class, 'show']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/product/{product}', [ProductController::class, 'show'])->name('product');
 
-Route::get('/admin/products', [AdminProductController::class, 'index']);
-Route::get('/admin/products/edit', [AdminProductController::class, 'edit']);
+Route::get('/admin/products', [AdminProductController::class, 'index'])->name('admin.products');
+Route::get('/admin/products/edit', [AdminProductController::class, 'edit'])->name('admin.products');
