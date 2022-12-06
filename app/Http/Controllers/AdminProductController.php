ae5f3c782c963c528123a40272d737c7863766d4
nicollas.lopes <nicollaslopesn@gmail.com>
2022-12-06 11:20:26 -0300

    edição e criação de produtos

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -2,15 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
 
 class AdminProductController extends Controller
 {
     public function index() {
-        return view('admin.products');
+        $products = Product::All();
+        
+        return view('admin.products', compact('products'));
     }
 
     public function edit() {
         return view('admin.products_edit');
+    }
+
+    public function update() {
+        return view();
+    }
+
+    public function create() {
+        return view('admin.products_create');
+    }
+
+    public function store(Request $request) {
+
+        $input = $request->validate([
+            'name' => 'string|required',
+            'price' => 'int|required',
+            'stock' => 'int|nullable',
+            'cover' => 'file|nullable',
+            'description' => 'string|nullable'
+        ]);
+
+        $input['slug'] = Str::slug($input['name']);
+
+        if (!empty($input['cover']) && $input['cover']->isValid()) {
+            $file = $input['cover'];
+            $path = $file->store('public/products');
+            $input['cover'] = $path;
+        }
+
+        Product::create($input);
+
+        return Redirect::route('admin.products');
+
     }
 }
